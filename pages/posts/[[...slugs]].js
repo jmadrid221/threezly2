e6dd94655346b51b369d957c0bce8424531964dd
Harish <87933016+unknownharish@users.noreply.github.com>
2022-12-02 23:21:36 +0530

    Update [[...slugs]].js

--- pages/posts/[[...slugs]].js
+++ pages/posts/[[...slugs]].js
@@ -47,7 +47,7 @@ export async function getStaticProps(Context) {
 
 
 
-    let data = await fetch('https://facetreatments.vercel.app/api/getMetadata', {
+    let data = await fetch('https://rupali.vercel.app/api/getMetadata', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
